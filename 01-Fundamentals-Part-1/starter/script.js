e840d679f15b81ddfb93be8482ad2a076dc9ba2e
kriystinne <anca.vasile@protonmail.com>
2020-10-29 12:49:29 +0100

    went through basic html and css

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,23 @@
+// The SWITCH statement
+
+/*const day = "Saturday";
+
+switch(day) {
+    case "Monday":
+        console.log(`Plan my course structure.`);
+        console.log("It is the first day of the week.");
+        break;
+    case "Tuesday":
+    case "Wednesday":
+        console.log("Chilling in the backyard.");
+        break;
+    default:
+        console.log("Drinking beer.");
+}*/
+
+// using STRICT MODE - more secure code (needs to be the first line)
+'use strict';
+
+// ARROW FUNCTIONS
+const calcAge = (birthYear) => 2037 - birthYear;
+const age = 1990;
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = dob;
+  }
+}
+
+const jessica = new Person('Jessica', 'Jones', '1990');
+console.log(jessica);
